0649f20ad3531f41c4108fe0f74d870306ab1f49
KARL ENANO <karlenano31@gmail.com>
2022-10-29 12:01:24 +0800

    Setup datasource &  user model, repo

---
+++ src/models/user.model.ts
@@ -0,0 +1,66 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({
+  //REVIEW added settings for custom collection name
+  settings: {
+    mongodb: {
+      collection: 'users',
+    },
+  },
+})
+export class User extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    mongodb: {dataType: 'ObjectId'},
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+    jsonSchema: {
+      format: 'email',
+    },
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  firstName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  lastName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  role: string;
+
+  @property({
+    type: 'boolean',
+  })
+  approved?: boolean;
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
