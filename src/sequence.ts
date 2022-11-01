9d176b82c587a09565e92209305c3f8630f2a6f0
KARL ENANO <karlenano31@gmail.com>
2022-11-01 17:09:33 +0800

    invoked middleware for CORS

--- src/sequence.ts
+++ src/sequence.ts
@@ -1,8 +1,11 @@
 import {AuthenticateFn, AuthenticationBindings} from '@loopback/authentication';
 import {inject} from '@loopback/core';
 import {
+  ErrorWriterOptions,
   FindRoute,
   InvokeMethod,
+  InvokeMiddleware,
+  LogError,
   ParseParams,
   Reject,
   RequestContext,
@@ -13,6 +16,9 @@ import {
 const SequenceActions = RestBindings.SequenceActions;
 
 export class MySequence implements SequenceHandler {
+  @inject(SequenceActions.INVOKE_MIDDLEWARE, {optional: true})
+  protected invokeMiddleware: InvokeMiddleware = () => false;
+
   constructor(
     @inject(SequenceActions.FIND_ROUTE) protected findRoute: FindRoute,
     @inject(SequenceActions.PARSE_PARAMS) protected parseParams: ParseParams,
@@ -21,12 +27,19 @@ export class MySequence implements SequenceHandler {
     @inject(SequenceActions.REJECT) public reject: Reject,
     @inject(AuthenticationBindings.AUTH_ACTION)
     protected authenticateRequest: AuthenticateFn,
+
+    @inject(RestBindings.SequenceActions.LOG_ERROR)
+    protected logError: LogError,
+    @inject(RestBindings.ERROR_WRITER_OPTIONS, {optional: true})
+    protected errorWriterOptions?: ErrorWriterOptions,
   ) {}
 
   //Top-level middleware
   async handle(context: RequestContext): Promise<void> {
     try {
       const {request, response} = context;
+      const finished = await this.invokeMiddleware(context);
+      if (finished) return;
       const route = this.findRoute(request);
       await this.authenticateRequest(request); //Verify JWT
       const args = await this.parseParams(request, route);
